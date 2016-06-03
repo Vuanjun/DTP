02af51707d5cbcc5322d8f828f9a649920281c41
Vuanjun <victor@meepshop.com>
2016-06-03 19:25:57 +0800

    card has an issue

--- app.js
+++ app.js
@@ -12,35 +12,35 @@ var router = require('koa-router');
 app.use(router(app));
 app.use(mount('/assets', serve(__dirname + '/assets')));
 
+app.get('/top', function *(next) {
+  var html = yield render('src/top.jade');
+  this.status = 200;
+  this.body = html;
+});
+
+app.get('/co-ltd', function *(next) {
+  var html = yield render('src/co-ltd.jade');
+  this.status = 200;
+  this.body = html;
+});
+
+app.get('/about', function *(next) {
+  var html = yield render('src/about.jade');
+  this.status = 200;
+  this.body = html;
+});
+
+app.get('/people', function *(next) {
+  var html = yield render('src/people.jade');
+  this.status = 200;
+  this.body = html;
+});
+
 app.get('/', function *(next) {
   var html = yield render('src/index.jade');
   this.status = 200;
   this.body = html;
 });
-
-app.get('/page-showcase', function *(next) {
-  var html = yield render('src/page-showcase.jade');
-  this.status = 200;
-  this.body = html;
-});
-
-app.get('/page-books', function *(next) {
-  var html = yield render('src/page-books.jade');
-  this.status = 200;
-  this.body = html;
-});
-
-app.get('/page-single-book-detail', function *(next) {
-  var html = yield render('src/page-single-book-detail.jade');
-  this.status = 200;
-  this.body = html;
-});
-
-// app.get('/pages/product', function *(next) {
-//   var html = yield render('src/pages/product.jade');
-//   this.status = 200;
-//   this.body = html;
-// });
 
 console.log('Hey sailor, Your server is on at port: 3333');
 app.listen(3333);
---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// var topLink = document.getElementById('top-link');
+// var secondNav = document.getElementById('second-nav');
+
+// topLink.addEventListener('click', function() {
+//   console.log('clicked');
+//     if (secondNav.classList.length == 1) {
+//       secondNav.classList.add('show');  
+//     }
+//     else {
+//       secondNav.classList.remove('show');     
+//     }
+// });
